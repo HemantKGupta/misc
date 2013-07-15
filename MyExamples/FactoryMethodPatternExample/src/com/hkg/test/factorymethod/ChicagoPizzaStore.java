a24f5490bd49266cbe2216c711bf5bc87efc006c
Hemant Gupta <rajni.hemant@gmail.com>
2013-07-15 22:00:36 +0530

    xSX

---
+++ MyExamples/FactoryMethodPatternExample/src/com/hkg/test/factorymethod/ChicagoPizzaStore.java
@@ -0,0 +1,12 @@
+package com.hkg.test.factorymethod;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+	Pizza createPizza(String item) {
+        	if (item.equals("cheese")) {
+            		return new ChicagoStyleCheesePizza();
+        	} else if (item.equals("veggie")) {
+        	    	return new ChicagoStyleVeggiePizza();
+        	} else return null;
+	}
+}
---
+++ MyExamples/FactoryMethodPatternExample/src/com/hkg/test/factorymethod/NYPizzaStore.java
@@ -0,0 +1,12 @@
+package com.hkg.test.factorymethod;
+
+public class NYPizzaStore extends PizzaStore {
+
+	Pizza createPizza(String item) {
+		if (item.equals("cheese")) {
+			return new NYStyleCheesePizza();
+		} else if (item.equals("veggie")) {
+			return new NYStyleVeggiePizza();
+		} else return null;
+	}
+}
